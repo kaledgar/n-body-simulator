24690622f9488fe955212ba3919405354956b3e4
kaledgar <kacperlucas672@gmail.com>
2024-09-15 15:26:03 +0200

    wip

--- nbody/__main__.py
+++ nbody/__main__.py
@@ -10,7 +10,7 @@ animation_space_index_step = 30  # choose every 10th for animation
 
 if __name__ == "__main__":
     plt.style.use("dark_background")
-
+    n = 3
     # Initialize three bodies: Earth, Sun, and a third body
     earth = Body(m=1, r_0=(1, 0), v_0=(0, np.sqrt(333000)), name="earth")
     sun = Body(m=333000, r_0=(0, 0), v_0=(0, 0), name="sun")
@@ -20,7 +20,7 @@ if __name__ == "__main__":
 
     # http://three-body.ipb.ac.rs/sol.php?id=13
     system = System(
-        Body(m=1, r_0=(-1, 0), v_0=(p1, p2), name="b1"),
+        Body(m=1, r_0=(-1, 0), v_0=(p1 * 1.1, p2), name="b1"),
         Body(m=1, r_0=(1, 0), v_0=(p1, p2), name="b2"),
         Body(m=1, r_0=(0, 0), v_0=(-2 * p1, -2 * p2), name="b3"),
     )
@@ -34,10 +34,34 @@ if __name__ == "__main__":
 
     # Solve the system's ODE
     sol = system.solve(t)
+    print(len(sol))
+    print(type(sol))
+    print(sol.shape)
+    print(sol)
 
     # Extract position data for all bodies from the solution
     num_bodies = len(system.bodies)
     positions = sol[:, : 2 * num_bodies]
+
+    def animate_evolution(solution_matrix: np.ndarray):
+        """
+        solution_matrix should be np.ndarray of shape: (number_of_time_points, m),
+        where m - even number
+
+        [
+            [ # x_1_list(t) ],
+            [ # y_1_list(t) ],
+            [ # x_2_list(t) ],
+            [ # y_2_list(t) ],
+                ...
+            [ # x_n_list(t) ],
+            [ # y_n_list(t) ],
+        ],
+
+        where: n - # bodies in the System
+        """
+        assert solution_matrix.shape[1] % 2 == 0
+        print("Ok")
 
     # Plot the trajectories of each body
     x1 = positions[:, 0]
@@ -46,6 +70,15 @@ if __name__ == "__main__":
     y2 = positions[:, 3]
     x3 = positions[:, 4]
     y3 = positions[:, 5]
+
+    plt.plot(x1, label="x1")
+    plt.plot(y1, label="y1")
+    plt.plot(x2, label="x2")
+    plt.plot(y2, label="y2")
+    plt.plot(x3, label="x3")
+    plt.plot(y3, label="y3")
+    plt.legend()
+    plt.show()
 
     print(len(x1))
     """
@@ -64,22 +97,25 @@ if __name__ == "__main__":
     ax.set_ylim(-2, 2)
     plt.grid(True, lw=0.3)
 
+    animated_plots_list: List[Tuple] = [(ax.plot([], []),) for _ in range(n)]
+
+    print(f"animated_plots_list = {animated_plots_list}")
+
     (animated_plot_l1,) = ax.plot([], [])
-    (animated_plot_p1,) = ax.plot([], [], "yellow", markersize=1)
     (animated_plot_l2,) = ax.plot([], [])
     (animated_plot_l3,) = ax.plot([], [])
 
     print(animated_plot_l1)
+
+    def automatic_animation(i):
+        for plot_tuple in animated_plots_list:
+            pass
 
     def animate(i):
         # plot lines
         animated_plot_l1.set_data(
             x1[0 : i * animation_space_index_step],
             y1[0 : i * animation_space_index_step],
-        )
-        animated_plot_p1.set_data(
-            x1[i * animation_space_index_step : i * animation_space_index_step],
-            y1[i * animation_space_index_step : i * animation_space_index_step],
         )
         animated_plot_l2.set_data(
             x2[0 : i * animation_space_index_step],
@@ -100,7 +136,7 @@ if __name__ == "__main__":
             x2[i * animation_space_index_step], y2[i * animation_space_index_step]
         )"""
 
-        return animated_plot_l1, animated_plot_l2, animated_plot_l3, animated_plot_p1
+        return animated_plot_l1, animated_plot_l2, animated_plot_l3
 
     animation = FuncAnimation(
         fig,
